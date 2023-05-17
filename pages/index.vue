2d154a309fb431677cc68d78a1bca925f877880b
Eduardo Braga <eduardocbraga@hotmail.com>
2023-05-17 14:25:10 -0300

    adiciona componente de cartao de produto

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
 	<div>
 		<h2 class="text-white text-2xl text-center my-8">All products</h2>
 		<div class="grid grid-cols-2 gap-7">
-			<div v-for="product in products">
-				{{product.title}}
+			<div v-for="product in products" :key="product.id">
+				<ProductCard :product="product"/>
 			</div>
 		</div>
 	</div>
