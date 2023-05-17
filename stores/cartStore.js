d1960aefdf451a978be80e9c7772a5e9ad7d4187
Eduardo Braga <eduardocbraga@hotmail.com>
2023-05-17 20:14:07 -0300

    botão para reduzir quantidade de produtos no carrinho

--- stores/cartStore.js
+++ stores/cartStore.js
@@ -40,6 +40,24 @@ export const useCartStore = defineStore('cart', {
 				method: 'put',
 				body: JSON.stringify(updatedProduct)
 			})
+		},
+		async decreaseQuantity(product) {
+			let updatedProduct
+			this.cart = this.cart.map(p => {
+				if (p.id === product.id && p.quantity > 1) {
+					p.quantity--
+					updatedProduct = p
+				}
+				return p
+			})
+
+			if (updatedProduct) {
+				//make put request
+				await $fetch('http://localhost:4000/cart/' + product.id, {
+					method: 'put',
+					body: JSON.stringify(updatedProduct)
+				})	
+			}
 		}
 	}
 })
