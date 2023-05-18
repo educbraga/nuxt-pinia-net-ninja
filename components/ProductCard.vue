a7060e501515805bdb70c5863dc703a4cc1b9333
Eduardo Braga <eduardocbraga@hotmail.com>
2023-05-18 08:55:49 -0300

    adiciona estado de carregamento ao botão de adicionar produto

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -5,8 +5,9 @@
 			<p class="text-2xl text-secondary">{{product.title}}</p>
 			<p class="text-xl text-gray-50">{{product.description}}</p>
 			<p class="text-lg text-secondary my-3">{{product.proce}} Silver coins</p>
-			<button class="btn" @click="addToBasket()">
-				<span>Add to Basket</span>
+			<button class="btn" @click="addToBasket()" :disabled="isPending">
+				<span v-show="!isPending">Add to Basket</span>
+				<span v-show="isPending">Adding...</span>
 			</button>
 		</div>
 	</div>
@@ -16,9 +17,15 @@
 
 	const {product} = defineProps(['product'])
 
+	const isPending = ref(false)
+
 	const cartStore = useCartStore()
 
 	const addToBasket = async () => {
+		isPending.value = true
 		await cartStore.addToCart(product)
+		setTimeout(()=>{
+			isPending.value = false
+		}, 1000)
 	}
 </script>
